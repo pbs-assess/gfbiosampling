fb76e841e891578b881b42324060a66c1f75a3be
Luke A. Rogers <luke.allister.rogers@gmail.com>
2022-06-02 17:59:29 -0700

    updated with tidy_commercial_counts() to tidy by area

---
+++ R/plot-commercial-counts.R
@@ -0,0 +1,12 @@
+
+# Adapted from gfplot::plot_sample_avail
+plot_commercial_counts <- function () {}
+
+
+
+# na_colour <- "white"
+#
+# plot_sample_avail(temp, title = en2fr("Commercial samples", french), year_range = c(1996, final_year_surv),
+#                   french = french) +
+#   ggplot2::ggtitle(en2fr("Commercial specimen counts", french)) +
+# viridis::scale_fill_viridis(option = "D", end = 0.82, na.value = na_colour)
